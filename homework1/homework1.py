dd78b95d5bc35b060c2b96ea795df28454785c48
Ava Garcia <avagarcia411@berkeley.edu>
2025-09-08 16:39:01 -0700

    saving homework1/folder

---
+++ homework1/homework1.py
@@ -0,0 +1,15 @@
+File: homework1. py
+# --- Variables and Data Types ---
+
+
+a=10
+print(a)
+print(type(a)) # a is an intiger, a whole number with no decimals
+
+b = 1.5
+print(b)
+print(type(b)) # b is a float, a number with decimals
+
+c = 3j
+print(c)
+print(type(c)) # c is 
